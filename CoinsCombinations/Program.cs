4e4d59f1a9891667db5f824a44b9e2ffffaf0310
Яхин Сергей <Neamiades@gmail.com>
2017-08-07 19:18:20 +0300

    Add project files.

---
+++ CoinsCombinations/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CoinsCombinations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
